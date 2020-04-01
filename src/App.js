af92a235aaf9405e41dc3d50bcd8477a80cc998f
HoSoo Lee <hosoo.lee@arithnea.de>
2020-04-02 00:20:48 +0200

    [01] The Basics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef, useEffect, useState } from 'react';
 import './App.css';
+import { select } from "d3";
+
 
 function App() {
+  const [data, setData] = useState([25, 30, 45, 60, 20]);
+  const svgRef = useRef();
+  useEffect(() => {
+    const svg = select(svgRef.current);
+    svg
+      .selectAll("circle")
+      .data(data)
+      .join(
+        enter => 
+          enter
+          .append("circle")
+          .attr("class", "new")
+          .attr("r", value => value)
+          .attr("cx", value => value * 2)
+          .attr("cy", value => value * 2)
+          .attr("stroke", "red"),
+        update => 
+          update
+            .attr("class", "updated")
+            .attr("r", value => value)
+            .attr("cx", value => value * 2)
+            .attr("cy", value => value * 2),
+        exit => exit.remove()
+      );
+  }, [data]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <svg ref={svgRef}></svg>
+      <br/>
+      <button onClick={()=> setData(data.map(value => value + 5))}>
+        Updata data
+      </button>
+      <button onClick={()=> setData(data.filter(value => value < 35))}>
+        Filter data
+      </button>
+    </React.Fragment> 
   );
 }
 
