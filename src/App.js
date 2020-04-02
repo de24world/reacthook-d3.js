499c027551f00955aa045284e07150e4e7e80c0b
HoSoo Lee <hosoo.lee@arithnea.de>
2020-04-03 00:07:38 +0200

    [02] Curved Line Chart

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
 import React, { useRef, useEffect, useState } from 'react';
 import './App.css';
-import { select } from "d3";
+import { select, line, curveCardinal } from "d3";
 
 
 function App() {
-  const [data, setData] = useState([25, 30, 45, 60, 20]);
+  const [data, setData] = useState([25, 30, 45, 60, 20, 65, 75]);
   const svgRef = useRef();
   useEffect(() => {
     const svg = select(svgRef.current);
+    const myLine = line()
+      .x((value, index) => index * 50)
+      .y(value => 150 - value)
+      .curve(curveCardinal);
+    // svg
+    //   .selectAll("circle")
+    //   .data(data)
+    //   .join("circle")
+    //   .attr("r", value => value)
+    //   .attr("cx", value => value * 2)
+    //   .attr("cy", value => value * 2)
+    //   .attr("stroke","red");
     svg
-      .selectAll("circle")
-      .data(data)
-      .join(
-        enter => 
-          enter
-          .append("circle")
-          .attr("class", "new")
-          .attr("r", value => value)
-          .attr("cx", value => value * 2)
-          .attr("cy", value => value * 2)
-          .attr("stroke", "red"),
-        update => 
-          update
-            .attr("class", "updated")
-            .attr("r", value => value)
-            .attr("cx", value => value * 2)
-            .attr("cy", value => value * 2),
-        exit => exit.remove()
-      );
+      .selectAll("path")
+      .data([data])
+      .join("path")
+      .attr("d", value => myLine(value))
+      .attr("fill", "none")
+      .attr("stroke", "blue");
   }, [data]);
+
   return (
     <React.Fragment>
-      <svg ref={svgRef}></svg>
+      <svg ref={svgRef}>
+        <path d="MO, 150 100,100 150,120" stroke="blue" fill="none" />>
+      </svg>
       <br/>
       <button onClick={()=> setData(data.map(value => value + 5))}>
         Updata data
